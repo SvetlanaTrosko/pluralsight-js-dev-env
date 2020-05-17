3749521d951e3fc623f43f167766cbf83bcbc201
strosko <strosko@globalblue.com>
2020-05-17 15:32:43 +0200

    Fixed no-console and no-debugger linting errors

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -4,7 +4,7 @@
     import webpack from 'webpack';
     import config from '../webpack.config.dev';
 
-/* eslint-disable-line no-console */
+
 
     const port = 3000;
     const app = express();
@@ -20,6 +20,15 @@
       res.sendFile(path.join(__dirname, '../src/index.html'));
     });
 
+
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
+});
 
     app.listen(port, function(err) {
       if (err) {
